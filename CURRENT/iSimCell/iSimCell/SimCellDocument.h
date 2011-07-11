50d244d140305800015a942e4d37ae150774e8ed
Davide Rambaldi <davide.rambaldi@ifom-ieo-campus.it>
2011-07-11 18:42:38 +0200

    Restarting From Scratch

---
+++ CURRENT/iSimCell/iSimCell/SimCellDocument.h
@@ -0,0 +1,16 @@
+//
+//  SimCellDocument.h
+//  iSimCell
+//
+//  Created by drambald on 7/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SimCellDocument : NSPersistentDocument {
+@private
+
+}
+
+@end
