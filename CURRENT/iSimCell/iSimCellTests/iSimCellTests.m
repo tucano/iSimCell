4a102fea2a0e8c379be507ff9db681628e96ec22
Davide Rambaldi <davide.rambaldi@ifom-ieo-campus.it>
2011-07-11 18:59:10 +0200

    new project

---
+++ CURRENT/iSimCell/iSimCellTests/iSimCellTests.m
@@ -0,0 +1,33 @@
+//
+//  iSimCellTests.m
+//  iSimCellTests
+//
+//  Created by drambald on 7/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "iSimCellTests.h"
+
+
+@implementation iSimCellTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iSimCellTests");
+}
+
+@end
